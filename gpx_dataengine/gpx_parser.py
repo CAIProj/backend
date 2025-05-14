c0b84373059da9a18aa8e7efd2ff9a2c48eb7b21
Prashilkumar-Rupapara <prashilkumar.rupapara@gmail.com>
2025-05-14 22:42:18 +0200

    staged commit

---
+++ gpx_dataengine/gpx_parser.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from pygeodesy import GeoidKarney
+from pygeodesy.ellipsoidalKarney import LatLon
+import gpxpy
+from typing import Optional
+
+
+@dataclass
+class Point:
+    "Represents a geographical point (log, lat, elevation)"
+    latitude: float
+    longitude: float
+    elevation: Optional[float] = None
+
+    def to_dict(self):
+        return {'latitude': self.latitude, 'longitude': self.longitude}
+    
+class GPXParser:
+    "Extracts point from GPX files"
+
+    @staticmethod
+    def parse_gpx_file(file_path: str) -> list[Point]:
+        try:
+            with open(file_path, 'r') as gpx_file:
+                gpx = gpxpy.parse(gpx_file)
+
+            points = []
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    for point in segment.points:
+                        geoid = GeoidKarney("egm2008-5.pgm")
+                        location = LatLon(point.latitude,point.longitude)
+                        height = geoid(location)
+                        points.append(Point(latitude=point.latitude,longitude=point.longitude,elevation=point.elevation-height))
+            
+            return points
+        except Exception:
+            raise
