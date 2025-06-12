039ed1604cd324fbc7510bc60729c6550555c7b9
Prashilkumar-Rupapara <prashilkumar.rupapara@gmail.com>
2025-06-12 15:18:17 +0200

    Moved `compare_elevation_apis()` to `compare_elevations.py`

--- src/gpx_dataengine/compare_elevations.py
+++ src/gpx_dataengine/compare_elevations.py
@@ -4,7 +4,47 @@ from scipy.interpolate import griddata
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 from models import ElevationProfile, Track
-from elevation_api import OpenElevationAPI
+from elevation_api import OpenElevationAPI, ElevationAPI
+
+
+# Standalone comparison plot using both APIs
+def compare_elevation_apis(gpx_file_path: str):
+    from plotter import Plotter  # Delayed import to avoid circular dependency with plotter.py
+
+    try:
+        track = Track.from_gpx_file(gpx_file_path)
+
+        # Get elevations from both apis
+        elevations_from_openelevation = OpenElevationAPI.get_elevations(track.points)
+        elevations_from_openstreetmap = ElevationAPI.get_elevations(track.points)
+
+        # Create profiles for api elevations
+        openelevation_elevation_profile = track.elevation_profile.copy()
+        openelevation_elevation_profile.set_elevations(elevations_from_openelevation) 
+
+        openstreetmap_elevation_profile = track.elevation_profile.copy()
+        openstreetmap_elevation_profile.set_elevations(elevations_from_openstreetmap)
+
+
+        # Plot comparision plots
+        plotter1 = Plotter()
+        plotter1.add_profiles(
+            (track.elevation_profile, 'From GPX file'),
+            (openelevation_elevation_profile, 'From Openelevation API'),
+        )
+        plotter1.plot_distance_vs_elevation()
+        
+        plotter2 = Plotter()
+        plotter2.add_profiles(
+            (track.elevation_profile, 'From GPX file'),
+            (openstreetmap_elevation_profile, 'From Openstreetmap API') 
+        )
+        plotter2.plot_distance_vs_elevation()
+        
+    except Exception as e:
+        print(f"An error occurred during comparison: {e}")
+        raise
+
 
 class Plot3D:
 
@@ -111,3 +151,12 @@ def plot3d(main_args):
         )
     except:
         raise
+    
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Compare elevation data from multiple APIs")
+    parser.add_argument("gpx_file", help="Path to the GPX file to compare elevations")
+    args = parser.parse_args()
+
+    compare_elevation_apis(args.gpx_file)
